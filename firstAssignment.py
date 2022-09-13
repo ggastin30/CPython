5b6fd48ccc154dbd21725e79b8748f9432ff390c
ggastin30 <ggastin30@charlottesvilleschools.org>
2022-09-13 09:41:06 -0400

    Stuff Updated

--- firstAssignment.py
+++ firstAssignment.py
@@ -1,5 +1,7 @@
+from time import time
 import board
 import neopixel
+import time
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
 dot.brightness = 0.5 
@@ -8,3 +10,6 @@ print("Make it red!")
 
 while True:
     dot.fill((255, 0, 0))
+    time.sleep(0.5)
+    dot.fill((0,0,0))
+    time.sleep(0.5)
