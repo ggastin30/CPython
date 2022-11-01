0e773829aa3f05e988c54066d796a64c6ada972f
ggastin30 <ggastin30@charlottesvilleschools.org>
2022-11-01 10:41:01 -0400

    up

---
+++ Engineering 3 Code/motorControl.py
@@ -0,0 +1,17 @@
+
+import time
+import board
+import simpleio
+from analogio import AnalogIn
+import adafruit_motor
+
+potentiometer = AnalogIn(board.A0)
+motor1 = motor.DCMotor(board. D9)
+
+print("hello world")
+
+while True:
+    print((potentiometer.value,)) 
+    ticks = potentiometer.value
+    spped = simpleio.map_range(ticks,0,1023,0,1)
+    motor1._throttle(spped)
