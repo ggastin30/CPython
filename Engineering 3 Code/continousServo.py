5fff7f4a036039745a730a2b2ea5d4e1ed79ac07
ggastin30 <ggastin30@charlottesvilleschools.org>
2022-12-06 09:54:35 -0500

    Servo works

--- Engineering 3 Code/continousServo.py
+++ Engineering 3 Code/continousServo.py
@@ -8,7 +8,7 @@ import board
 import pwmio
 from adafruit_motor import servo
 
-# create a PWMOut object on Pin A2.
+# create a PWMOut object on Pin A1.
 pwm = pwmio.PWMOut(board.A1, frequency=50)
 
 # Create a servo object, my_servo.
@@ -17,13 +17,5 @@ my_servo = servo.ContinuousServo(pwm)
 while True:
     print("forward")
     my_servo.throttle = 1.0
-    time.sleep(2.0)
-    print("stop")
-    my_servo.throttle = 0.0
-    time.sleep(2.0)
-    print("reverse")
-    my_servo.throttle = -1.0
-    time.sleep(2.0)
-    print("stop")
-    my_servo.throttle = 0.0
-    time.sleep(4.0)
+    time.sleep(100.0)
+   
