5872a9c9f2c8697c5d9e7344b9164e95d4a1a51c
ggastin30 <ggastin30@charlottesvilleschools.org>
2022-12-09 09:17:07 -0500

    m

--- Engineering 3 Code/continousServo.py
+++ Engineering 3 Code/continousServo.py
@@ -8,14 +8,13 @@ import board
 import pwmio
 from adafruit_motor import servo
 
-# create a PWMOut object on Pin A1.
-pwm = pwmio.PWMOut(board.A1, frequency=50)
+# create a PWMOut object on Pin D2.
+pwm = pwmio.PWMOut(board.D3, frequency=50)
+#pwmTop = pwmio.PWMOut(board.A2, frequency=50)
 
 # Create a servo object, my_servo.
 my_servo = servo.ContinuousServo(pwm)
-
+#my_servoTop = servo.ContinuousServo(pwmTop)
 while True:
     print("forward")
     my_servo.throttle = 1.0
-    time.sleep(100.0)
-   
--- Engineering 3 Code/servoAssignment.py
+++ Engineering 3 Code/servoAssignment.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
 #ctrl alt r
 # SPDX-License-Identifier: MIT
+
 """CircuitPython Essentials Servo standard servo example"""
 import time
 import board
@@ -8,7 +9,7 @@ import pwmio
 from adafruit_motor import servo
 
 # create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.D2, duty_cycle=2 ** 15, frequency=250)
+pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
 
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
@@ -16,7 +17,9 @@ my_servo = servo.Servo(pwm)
 while True:
     for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
         my_servo.angle = angle
-        time.sleep(0.01)
+        time.sleep(0.05)
+        print("left")
     for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
         my_servo.angle = angle
-        time.sleep(0.01)
+        time.sleep(0.05)
+        print("right")
