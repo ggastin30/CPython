e2c5070bec3749de1c0b259600a3ee707e22bd15
ggastin30 <ggastin30@charlottesvilleschools.org>
2023-05-01 09:51:53 -0400

    g

--- Engineering 3 Code/fanPID.py
+++ Engineering 3 Code/fanPID.py
@@ -10,16 +10,27 @@ photoI.pull = digitalio.Pull.UP
 
 #Set up a digital input for the photo interrupter and enable its pull-up resistor
 
-last_photoI = True
+last_I = 0
+new_I = 0
+since_I = 0
+State = 1
+rpm = 0
 
-interrupts = 0
 
 while True:
-    if photoI.value == True & last_photoI == True:
-        interrupts = interrupts + 1
-        last_photoI = False
-    if last_photoI == False:
-        time.sleep(.01)
-        last_photoI = True
-    print(interrupts)
-    time.sleep(.001)
+    while State == 1: #first trigger
+        if photoI.value == True: #if triggered
+            last_I = time.monotonic()
+            time.sleep(.05)
+            State = 2
+    while State == 2: #second trigger
+        if photoI.value == True: #if triggered
+                new_I = time.monotonic()
+                State = 3
+
+    while State == 3: #computation
+            since_I = new_I - last_I
+            rpm = 60/since_I
+            print(rpm)
+            time.sleep(.05)
+            State = 1    
