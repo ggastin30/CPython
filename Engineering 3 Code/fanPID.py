f588851ae93fcdbed89f87250ec92e8b6897ce5e
Grant <91094422+ggastin30@users.noreply.github.com>
2023-06-01 11:56:30 -0400

    done w/ LCD

--- Engineering 3 Code/fanPID.py
+++ Engineering 3 Code/fanPID.py
@@ -24,12 +24,20 @@ last_position = 0
 btn = digitalio.DigitalInOut(board.D1)
 btn.direction = digitalio.Direction.INPUT
 btn.pull = digitalio.Pull.UP
-Enstate = 400
+Enstate = 700
 
 Fan = analogio.AnalogOut(board.A1)
 
-#i2c = board.I2C()
-#lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
+lcdPower = digitalio.DigitalInOut(board.D9) #connects the lcd to pin 9
+lcdPower.direction = digitalio.Direction.INPUT #sets the lcd power flow as input
+lcdPower.pull = digitalio.Pull.DOWN #Pulls the power of the lcd down to ground
+while lcdPower.value is False: #creates an infinite loop that repeats until the lcd turns on
+    print("zzz")
+    time.sleep(0.1)
+print("I'm awake")
+i2c = board.I2C()
+lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
+
 
 last_I = 0
 new_I = 0
@@ -44,16 +52,19 @@ G = 0 #Debounce Variable
 
 while True:
     while State == 0:
-        Fan.value = int(simpleio.map_range(output, 0, 1150, 27500, 29000))
+        Fan.value = int(simpleio.map_range(output, 0, 1200, 27500, 29000))
         #Fan.value = 28750 #27500(300) - 28750(1100)
         if btn.value == False:
             time.sleep(.5)
-            print("MENU")
+            lcd.clear()
+            lcd.set_cursor_pos(0, 0)
+            lcd.print("MENU MODE       ")
             State = 4
             break
         #print(btn.value)
         if time.monotonic() > oldTime +.1:
-            #print("PID")
+            lcd.set_cursor_pos(0, 0)
+            lcd.print("PID MODE        ")
             State = 1
     while State == 1:  # first trigger
         if photoI.value is True:  # if triggered
@@ -68,6 +79,9 @@ while True:
         since_I = new_I - last_I
         rpm = 60/since_I
         output = pid(rpm)
+        RPM = int(rpm)
+        lcd.set_cursor_pos(1, 0)
+        lcd.print("RPM:" + str(RPM) + " Set:" + str(sP))
         print((rpm, rpm, sP))
         # lcd.print(rpm)
         oldTime = time.monotonic()
@@ -76,13 +90,17 @@ while True:
         position = encoder.position
         if position != last_position:
             if (position > last_position) & (G == 0):
-                Enstate = Enstate - 10
-                print(Enstate)
+                if Enstate > 500:
+                    Enstate = Enstate - 10
+                lcd.set_cursor_pos(1, 0)
+                lcd.print("ChangeSet: " + str(Enstate))
                 G = 1
                 last_position = position
             elif (position < last_position) & (G == 0):
-                Enstate = Enstate + 10
-                print(Enstate)
+                if Enstate < 1100:
+                    Enstate = Enstate + 10
+                lcd.set_cursor_pos(1, 0)
+                lcd.print("ChangeSet: " + str(Enstate))
                 G = 1
                 last_position = position
             G = 0
@@ -92,4 +110,5 @@ while True:
             time.sleep(.5)
             sP = Enstate
             pid.setpoint = sP
+            lcd.clear()
             State = 1
